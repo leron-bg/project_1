dff2c7d040ce1156db0e1423be3bb38f32b89ced
Dimitar Vladimirov Valkanov <dvalk@softserveinc.com>
2019-03-25 11:59:23 +0200

    RSA encryption

---
+++ Encryption/Encryption/Assymetric.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace Encryption
+{
+	public class Assymetric
+	{
+		public byte[] Encrypt(byte[] data, RSAParameters RSAKeyInfo)
+		{
+			byte[] encrypted;
+			using (var rsa = new RSACryptoServiceProvider())
+			{
+				rsa.ImportParameters(RSAKeyInfo);
+				encrypted = rsa.Encrypt(data, true);
+			}
+
+			return encrypted;
+		}
+
+		public byte[] Decrypt(byte[] data, RSAParameters RSAKeyInfo)
+		{
+			byte[] decrypted;
+			using (var rsa = new RSACryptoServiceProvider())
+			{
+				rsa.ImportParameters(RSAKeyInfo);
+				decrypted = rsa.Decrypt(data, true);
+			}
+
+			return decrypted;
+		}
+	}
+}
---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Encryption
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var assymetric = new Assymetric();
+			var data = new byte[] { 1, 2, 3, 4, 5 };
+
+			using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+			{
+				var encrypted = assymetric.Encrypt(data, rsa.ExportParameters(false));
+				Console.WriteLine("Encrypted data:");
+				var output = string.Join(",", encrypted);
+				Console.WriteLine(output);
+
+				Console.WriteLine("Decrypted data:");
+				Console.WriteLine(string.Join(",", assymetric.Decrypt(encrypted, rsa.ExportParameters(true))));
+			}
+		}
+	}
+}
