be72d07df1eb956ac0df4bd60ca106bc9b2ba0ef
Dimitar Vladimirov Valkanov <dvalk@softserveinc.com>
2019-04-01 11:46:01 +0300

    Initial commit

---
+++ SportsStore/SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b49b7d02-850b-41ac-9bb8-b69504c0b827")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsStore.WebUI.Infrastructure
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Web.Mvc;
+	using Ninject;
+
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		private IKernel _kernel;
+		public NinjectDependencyResolver(IKernel kernel)
+		{
+			_kernel = kernel;
+			AddBindings();
+		}
+		public object GetService(Type serviceType)
+		{
+			return _kernel.TryGet(serviceType);
+		}
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return _kernel.GetAll(serviceType);
+		}
+		private void AddBindings()
+		{
+			// put bindings here
+		}
+	}
+}
